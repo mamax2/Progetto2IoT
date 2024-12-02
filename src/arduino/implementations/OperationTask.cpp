57c9c3ebc87aca27491460f0fc22fb55887879a5
Giacomo Ruscelli <giacomo.ruscelli03@gmail.com>
2024-12-02 14:25:50 +0100

    fixed merging bugs

--- src/arduino/implementations/OperationTask.cpp
+++ src/arduino/implementations/OperationTask.cpp
@@ -9,6 +9,7 @@ void OperationTask::init(HardwareManager* hw, SerialManager* sm) {
     serial = sm;
     setupFlag = true; //flag usato per il setup degli stati, se lo stato ha bisogno di scrivere su lcd e accendere/spegnere led
     problemFlag = false; //flag usato per bloccare la task quando si verifica un problema
+    emptyWasteLevel = hardware->getWasteLevel();
 }
 
 void OperationTask::tick() {
@@ -80,43 +81,93 @@ void OperationTask::open(){
     }
 
     if(stateStartTime + openTime > millis() || digitalRead(hardware->BUTTON_CLOSE) == LOW)
-    { // if "openTime" seconds pass without user interaction (button close click) the door automatically close or the user click button close
-        currentState = close();
-        setupFlag = false;
+    {   //if "openTime" seconds pass without user interaction (button close click) the door automatically close or the user click button close
+        currentState = CLOSE;
+        setupFlag = true;
+    }
+
+    if(hardware->getWasteLevel() >= maximumWasteLevel)
+    {   //if bin get full -> go to full state and wait for operator emptying process
+        currentState = FULL;
+        setupFlag = true;
     }
 }
 
 void OperationTask::sleeping(){
-    if (hardware->isUserDetected()) {
+    if(setupFlag)
+    {   //sleeping state setup, clearing lcd
+        hardware->displayMessage("", "");
+        setupFlag = false;
+    }
+    
+    if (hardware->isUserDetected()) 
+    {   //if user is detected go back to idle
         currentState = IDLE;
+        setupFlag = true;
     }
 }
 
 void OperationTask::close(){
-    if (digitalRead(hardware->BUTTON_CLOSE) == LOW || millis() - stateStartTime > 10000) {
+    if(setupFlag)
+    {   //close state setup, closing door and displaying message
+        hardware->displayMessage("WASTE RECEIVED","THANK YOU");
         hardware->closeDoor();
-        hardware->displayMessage("WASTE RECEIVED", "THANK YOU!");
-        delay(2000);
-        currentState = IDLE;
+        setupFlag = false;
+        stateStartTime = millis();
     }
+
+    if(stateStartTime + closeTime > millis())
+    {   //when T2 time pass the task will go back to idle
+        currentState = IDLE;
+        setupFlag = true;
+    }
+    
 }
 
 void OperationTask::emptying(){
-    hardware->reverseDoor();
-    currentState=IDLE;
+    if(setupFlag)
+    {   //emptying task setup, displaying message and reversing door
+        hardware->displayMessage("EMPTYING IN","PROGRESS");
+        hardware->reverseDoor();
+        setupFlag = false;
+        stateStartTime = millis();
+    }
+    
+    if(stateStartTime + emptyingTime > millis())
+    {   //after t3 seconds the door close and go back to idle state
+        hardware->closeDoor();
+        currentState=IDLE;
+        setupFlag = true;
+    }
 }
 
 void OperationTask::full(){
-    hardware->setGreenLED(false);
-    hardware->setRedLED(true);
-    hardware->displayMessage("CONTAINER FULL", "");
+    if(setupFlag)
+    {   //full state setup, displaying message and closing the door, setting the led as requested
+        hardware->displayMessage("CONTAINER FULL","");
+        hardware->closeDoor();
+        hardware->setGreenLED(false);
+        hardware->setRedLED(true);
+        setupFlag = false;
+    }
+
+    if(emptyFlag)
+    {   //if empty flag is turned true by the operator from the dashboard the container go to emptying state
+        currentState = EMPTYING;
+        setupFlag = true;
+        emptyFlag = false;
+    }
 }
 
+//function used to access problem flag from problemTask
 void OperationTask::setProblemFlag(bool value){
     problemFlag = value;
     return;
 }
 
-bool OperationTask::getProblemFlag(){
-    return problemFlag;
+//function used to access empty flag from serial manager
+void OperationTask::emptyContainer(){
+    if(currentState == FULL){
+        emptyFlag = true;
+    }
 }
